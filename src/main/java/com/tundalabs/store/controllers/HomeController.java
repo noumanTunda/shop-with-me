461b9e85a3b370d7253121e6f63696c8bb308cea
noumanTunda <ntunda4000@gmail.com>
2026-08-13 15:54:07 +0300

    Feat: Added Thymleaf and Hello Endpoint

--- src/main/java/com/tundalabs/store/controllers/HomeController.java
+++ src/main/java/com/tundalabs/store/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.tundalabs.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String hone(){
-        return "index.html";
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("name", "Nouman");
+        return "index";
     }
 }
