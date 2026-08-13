a81f99aafa82d3b4eebbb1f2ba2d59c8f86016d1
noumanTunda <ntunda4000@gmail.com>
2026-08-13 16:29:33 +0300

    feat: Added Status Code for NotFound Users

--- src/main/java/com/tundalabs/store/controllers/UserController.java
+++ src/main/java/com/tundalabs/store/controllers/UserController.java
@@ -3,21 +3,31 @@ package com.tundalabs.store.controllers;
 import com.tundalabs.store.entities.User;
 import com.tundalabs.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 @AllArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
 //    public UserController(UserRepository userRepository) {
 //        this.userRepository = userRepository;
 //    }
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers(){
         return userRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
