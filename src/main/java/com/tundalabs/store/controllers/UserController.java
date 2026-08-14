9e9c76353c5683493391ad022853b0f7fadca3dc
noumanTunda <ntunda4000@gmail.com>
2026-08-14 14:40:42 +0300

    feat: Added Business Logic Validation to Register users only if Email is unique

--- src/main/java/com/tundalabs/store/controllers/UserController.java
+++ src/main/java/com/tundalabs/store/controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -50,10 +51,17 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(
+    public ResponseEntity<?> registerUser(
             @Valid
             @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder){
+
+        if(userRepository.existsByEmail(request.getEmail())){
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email Already Exists")
+            );
+        }
+
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 
