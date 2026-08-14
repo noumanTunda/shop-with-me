f7a00ea88def67ddfc10f3f8e38ec889c6ce0b28
noumanTunda <ntunda4000@gmail.com>
2026-08-14 11:29:29 +0300

    feat: Added Endpoint to User's Password

--- src/main/java/com/tundalabs/store/controllers/UserController.java
+++ src/main/java/com/tundalabs/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.tundalabs.store.controllers;
 
+import com.tundalabs.store.dtos.ChangePasswordRequest;
 import com.tundalabs.store.dtos.RegisterUserRequest;
 import com.tundalabs.store.dtos.UpdateUSerRequest;
 import com.tundalabs.store.dtos.UserDto;
@@ -7,6 +8,7 @@ import com.tundalabs.store.mappers.UserMapper;
 import com.tundalabs.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -83,5 +85,24 @@ public class UserController {
 
         userRepository.delete(user);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping({"/{id}/change-password"})
+    public ResponseEntity<Void> changePassword(
+        @PathVariable Long id,
+        @RequestBody ChangePasswordRequest request){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        if(!user.getPassword().equals(request.getOldPassword())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+
+        return ResponseEntity.noContent().build();
+
     }
 }
