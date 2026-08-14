cd4b5da14457547ef9d2688650eccd92fb8f4706
noumanTunda <ntunda4000@gmail.com>
2026-08-14 12:27:52 +0300

    feat: Added Endpoint to Create Product and Update Product

--- src/main/java/com/tundalabs/store/controllers/ProductController.java
+++ src/main/java/com/tundalabs/store/controllers/ProductController.java
@@ -3,10 +3,12 @@ package com.tundalabs.store.controllers;
 import com.tundalabs.store.dtos.ProductDto;
 import com.tundalabs.store.entities.Product;
 import com.tundalabs.store.mappers.ProductMapper;
+import com.tundalabs.store.repositories.CategoryRepository;
 import com.tundalabs.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -17,15 +19,16 @@ import java.util.List;
 public class ProductController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping()
     public List<ProductDto> getAllProducts(
-           @RequestParam(name = "categoryId", required = false) Byte categoryId
-    ){
+            @RequestParam(name = "categoryId", required = false) Byte categoryId
+    ) {
         List<Product> products;
-        if(categoryId != null){
+        if (categoryId != null) {
             products = productRepository.findByCategoryId(categoryId);
-        }else{
+        } else {
             products = productRepository.findAllWithCategory();
         }
         return products.stream()
@@ -33,21 +36,56 @@ public class ProductController {
                 .toList();
     }
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<ProductDto> getProduct(){
-//        var product = productRepository.findById(id).orElse(null);
-//        if(product == null) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        return ResponseEntity.ok(productMapper.toDto(product));
-//    }
     @GetMapping("/{id}")
-    public ResponseEntity<ProductDto> getProduct(@PathVariable Long id){
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long id) {
         var product = productRepository.findById(id).orElse(null);
-        if(product == null){
+        if (product == null) {
             return ResponseEntity.notFound().build();
         }
 
         return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto productDto,
+            UriComponentsBuilder uriBuilder) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if(category ==null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        var uri = uriBuilder.path("/products/{id}").buildAndExpand(productDto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(productDto);
+    }
+
+    @PutMapping({"/{id}"})
+    public ResponseEntity<ProductDto> updateProduct(
+        @PathVariable Long id,
+        @RequestBody ProductDto productDto){
+
+            var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+            if(category == null){
+                return ResponseEntity.badRequest().build();
+            }
+
+            var product = productRepository.findById(id).orElse(null);
+            if(product == null){
+                return ResponseEntity.notFound().build();
+            }
+
+            productMapper.update(productDto, product);
+            product.setCategory(category);
+            productRepository.save(product);
+            productDto.setId(product.getId());
+
+
+            return ResponseEntity.ok(productDto);
     }
 }
