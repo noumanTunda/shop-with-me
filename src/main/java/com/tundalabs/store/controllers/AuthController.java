b847fba1137b35a5efe8e82deeb9103a5e7509aa
noumanTunda <ntunda4000@gmail.com>
2026-08-17 09:33:30 +0300

    feat: Added JWT and Environment Variables

--- src/main/java/com/tundalabs/store/controllers/AuthController.java
+++ src/main/java/com/tundalabs/store/controllers/AuthController.java
@@ -1,28 +1,27 @@
 package com.tundalabs.store.controllers;
 
+import com.tundalabs.store.dtos.JwtResponse;
 import com.tundalabs.store.dtos.LoginRequest;
-import com.tundalabs.store.repositories.UserRepository;
+import com.tundalabs.store.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(
+    public ResponseEntity<JwtResponse> login(
            @Valid @RequestBody LoginRequest request
     ){
         authenticationManager.authenticate(
@@ -32,6 +31,14 @@ public class AuthController {
                 )
         );
 
-       return ResponseEntity.ok().build();
+        var token = jwtService.generateToken(request.getEmail());
+
+       return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
     }
 }
---
+++ src/main/java/com/tundalabs/store/services/JwtService.java
@@ -0,0 +1,25 @@
+package com.tundalabs.store.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+    public String generateToken(String email){
+        final long tokenExpiration = 86400; // 1 Day
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+}
