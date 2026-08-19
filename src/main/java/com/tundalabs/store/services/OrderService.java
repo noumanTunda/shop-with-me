b20f41f6def973d5956ecbaf46c724fc4a588e27
noumanTunda <ntunda4000@gmail.com>
2026-08-19 10:07:19 +0300

    feat: Added Endpoint to Get Order Placed By Specific Customer

--- src/main/java/com/tundalabs/store/services/OrderService.java
+++ src/main/java/com/tundalabs/store/services/OrderService.java
@@ -1,9 +1,11 @@
 package com.tundalabs.store.services;
 
 import com.tundalabs.store.dtos.OrderDto;
+import com.tundalabs.store.exceptions.OrderNotFoundException;
 import com.tundalabs.store.mappers.OrderMapper;
 import com.tundalabs.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +19,22 @@ public class OrderService {
 
     public List<OrderDto> getAllOrders(){
         var user = authService.getCurrentUser();
-        var orders = orderRepository.getAllByCustomer(user);
+        var orders = orderRepository.getOrdersByCustomer(user);
         return orders.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrder(Long orderId) {
+        var order = orderRepository.getOrderWithItems(orderId).orElse(null);
+        if(order == null){
+            throw new OrderNotFoundException();
+        }
+
+        var user = authService.getCurrentUser();
+        if(!order.isPlacedBy(user)){
+            throw new AccessDeniedException("You Dont have access to this Order");
+        }
+
+        return orderMapper.toDto(order);
+
     }
 }
